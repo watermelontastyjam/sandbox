d8f62451c3ac4c00882826398b7a2461af57ee19
Валеев <tastyjam4human@gmail.com>
2024-05-24 16:15:34 +0400

    adding sqlite

--- PigeonMail/app/src/main/java/ru/ssau/pigeonmail/data/MessengerContract.java
+++ PigeonMail/app/src/main/java/ru/ssau/pigeonmail/data/MessengerContract.java
@@ -10,9 +10,34 @@ public class MessengerContract {
     }
     public static final class Users implements BaseColumns{
         public final static String TABLE_NAME = "Users";
-//
-//        public final static String USER_ID = BaseColumns._ID;
-//        public final static String
 
+        public final static String USER_ID = BaseColumns._ID;
+        public final static String EMAIL = "email";
+        public final static String USER_NAME = "userName";
+        public final static String PROFILE_IMAGE = "profileImage";
+    }
+    public static final class UserChats implements BaseColumns{
+        public final static String TABLE_NAME = "User_chats";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String USER_ID = "userId";
+        public final static String CHAT_ID = "chatId";
+    }
+    public static final class Chats implements BaseColumns{
+        public final static String TABLE_NAME = "Chats";
+
+        public final static String CHAT_ID = BaseColumns._ID;
+        public final static String USER_1 = "user1";
+        public final static String USER_2 = "user2";
+
+    }
+    public static final class Messages implements BaseColumns{
+        public final static String TABLE_NAME = "Messages";
+
+        public final static String MESSAGE_ID = BaseColumns._ID;
+        public final static String CHAT_ID = "chatId";
+        public final static String DATE = "date";
+        public final static String OWNER_ID = "ownerId";
+        public final static String TEXT = "text";
     }
 }
---
+++ PigeonMail/app/src/main/java/ru/ssau/pigeonmail/data/MessengerDbHelper.java
@@ -0,0 +1,49 @@
+package ru.ssau.pigeonmail.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MessengerDbHelper extends SQLiteOpenHelper {
+    public final static String LOG_TAG = MessengerDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "messenger.db";
+    private static final int DATABASE_VERSION = 1;
+    public MessengerDbHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_USERS_TABLE = "CREATE TABLE " + MessengerContract.Users.TABLE_NAME + " ("
+                + MessengerContract.Users.USER_ID + "TEXT PRIMARY KEY, "
+                + MessengerContract.Users.EMAIL + "TEXT NOT NULL, "
+                + MessengerContract.Users.PROFILE_IMAGE + "TEXT, "
+                + MessengerContract.Users.USER_NAME + "TEXT NOT NULL);";
+
+        String SQL_CREATE_USER_CHATS_TABLE ="CREATE TABLE " + MessengerContract.UserChats.TABLE_NAME + " ("
+                + MessengerContract.UserChats._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + MessengerContract.UserChats.USER_ID + "TEXT NOT NULL, "
+                + MessengerContract.UserChats.CHAT_ID + "TEXT);";
+
+        String SQL_CREATE_CHATS_TABLE = "CREATE TABLE " + MessengerContract.Chats.TABLE_NAME + " ("
+                + MessengerContract.Chats.CHAT_ID + "TEXT PRIMARY KEY AUTOINCREMENT, "
+                + MessengerContract.Chats.USER_1 + "TEXT NOT NULL, "
+                + MessengerContract.Chats.USER_2 + "TEXT NOT NULL);";
+        String SQL_CREATE_MESSAGES_TABLE = "CREATE TABLE " + MessengerContract.Messages.TABLE_NAME + " ("
+                + MessengerContract.Messages.MESSAGE_ID+ "INTEGER PRIMARY KEY, "
+                + MessengerContract.Messages.CHAT_ID + "TEXT NOT NULL, "
+                + MessengerContract.Messages.DATE + "TEXT, "
+                + MessengerContract.Messages.OWNER_ID + "TEXT NOT NULL,"
+                + MessengerContract.Messages.TEXT + "TEXT);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_USERS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_USER_CHATS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_CHATS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_MESSAGES_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
